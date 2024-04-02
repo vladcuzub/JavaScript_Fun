2353a661e45847fc94dffa482ab3f64ef2a9739b
vladcuzub <vladalex1989@gmail.com>
2024-04-02 12:50:29 +0200

    add variables to Basisc lesson

--- c01_basisc/basics-01-starting-project/assets/scripts/app.js
+++ c01_basisc/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert('hello')
+// Variablen sind Containers zum Specichern von Daten
+
+let userName = 'Vlad' //let variablen kann neu zugewiesen werden
+userName = 'Jhonny'
+
+const surname = 'Cuzub' // const variablen kann nicht neu zugewiesen werden
+
+//Operators 
