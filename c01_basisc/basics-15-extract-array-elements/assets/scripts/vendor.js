674b87f16877c12c2b3157207d60cd06769dfab5
vladcuzub <vladalex1989@gmail.com>
2024-04-10 23:54:10 +0200

    formating some code

--- c01_basisc/basics-15-extract-array-elements/assets/scripts/vendor.js
+++ c01_basisc/basics-15-extract-array-elements/assets/scripts/vendor.js
@@ -3,7 +3,6 @@ const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
-
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
 
