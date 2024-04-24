44d4344910f8d32e22b5ddd98f158a8f2dd35ccf
vladcuzub <vladalex1989@gmail.com>
2024-04-24 23:47:38 +0200

    add functions like multipy,divide,substract

--- C02_functions/basics-06-basic-function/assets/scripts/app.js
+++ C02_functions/basics-06-basic-function/assets/scripts/app.js
@@ -1,11 +1,39 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-    const result = currentResult + userInput.value;
-   return result
+function getUserInput() {
+    return parseInt(userInput.value)
 }
 
-addBtn.addEventListener('click', add) //execute function 'indirectly' using DOM  (Document Object Model)
 
-outputResult(currentResult, calculationDescription);
+function add() {
+    const enteredNumber = getUserInput()
+    const calcDescription = `${currentResult} + ${enteredNumber}`
+    currentResult = currentResult + enteredNumber; // parseInt - convert string to a number 
+    outputResult(currentResult, calcDescription);
+}
+
+function subtract(){
+    const enteredNumber = getUserInput()
+    const calcDescription = `${currentResult} - ${enteredNumber}`
+    currentResult = currentResult - enteredNumber; 
+    outputResult(currentResult, calcDescription);
+}
+
+function multiply() {
+    const enteredNumber = getUserInput()
+    const calcDescription = `${currentResult} * ${enteredNumber}`
+    currentResult = currentResult * enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+function divide() {
+    const enteredNumber = getUserInput()
+    const calcDescription = `${currentResult} - ${enteredNumber}`
+    currentResult = currentResult - enteredNumber;
+    outputResult(currentResult, calcDescription);
+}
+
+
+addBtn.addEventListener('click', add) //execute function 'indirectly' using DOM  (Document Object Model)
+subtractBtn.addEventListener('click', subtract)
