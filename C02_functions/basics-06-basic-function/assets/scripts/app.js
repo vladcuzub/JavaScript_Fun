b8dfd487761d8bcffe270f1436315d0c0c7a8290
vladcuzub <vladalex1989@gmail.com>
2024-04-25 10:33:20 +0200

    refactoring functions

--- C02_functions/basics-06-basic-function/assets/scripts/app.js
+++ C02_functions/basics-06-basic-function/assets/scripts/app.js
@@ -2,38 +2,48 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function getUserInput() {
-    return parseInt(userInput.value)
+    return parseInt(userInput.value) // parseInt - convert string to a number 
 }
 
+function crateAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription);
+}
 
 function add() {
     const enteredNumber = getUserInput()
-    const calcDescription = `${currentResult} + ${enteredNumber}`
-    currentResult = currentResult + enteredNumber; // parseInt - convert string to a number 
-    outputResult(currentResult, calcDescription);
+    const initialResult = currentResult
+    currentResult = currentResult + enteredNumber;
+    crateAndWriteOutput('+', initialResult, enteredNumber)
+
 }
 
-function subtract(){
+function subtract() {
     const enteredNumber = getUserInput()
-    const calcDescription = `${currentResult} - ${enteredNumber}`
-    currentResult = currentResult - enteredNumber; 
-    outputResult(currentResult, calcDescription);
+    const initialResult = currentResult
+    currentResult = currentResult - enteredNumber;
+    crateAndWriteOutput('-', initialResult, enteredNumber)
+
 }
 
 function multiply() {
     const enteredNumber = getUserInput()
-    const calcDescription = `${currentResult} * ${enteredNumber}`
+    const initialResult = currentResult
     currentResult = currentResult * enteredNumber;
-    outputResult(currentResult, calcDescription);
+    crateAndWriteOutput('*', initialResult, enteredNumber)
+
 }
 
 function divide() {
     const enteredNumber = getUserInput()
-    const calcDescription = `${currentResult} - ${enteredNumber}`
-    currentResult = currentResult - enteredNumber;
-    outputResult(currentResult, calcDescription);
+    const initialResult = currentResult
+    currentResult = currentResult / enteredNumber;
+    crateAndWriteOutput('/', initialResult, enteredNumber)
+
 }
 
 
 addBtn.addEventListener('click', add) //execute function 'indirectly' using DOM  (Document Object Model)
 subtractBtn.addEventListener('click', subtract)
+multiplyBtn.addEventListener('click', multiply)
+divideBtn.addEventListener('click', divide)
