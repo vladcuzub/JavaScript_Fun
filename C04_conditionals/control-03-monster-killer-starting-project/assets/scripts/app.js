bfea2783101bf3ebc9560661c97bd1e26669f1e0
vladcuzub <vladalex1989@gmail.com>
2024-04-29 23:50:24 +0200

    add logic function

--- C04_conditionals/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ C04_conditionals/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 10;
 
 let chosenMaxLife = 100;
@@ -19,9 +20,23 @@ function attackHandler() {
         alert('You Won!')
     } else if (currentUserHealth <= 0){
         alert('You Lost!')
-       
     }
-
 }
 
-attackBtn.addEventListener('click', attackHandler)
+function strongAttackHandler () {
+    const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+
+    const playerDamge = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+    currentUserHealth -= playerDamge
+
+    if (currentMonsterHealth <= 0) {
+        alert('You Won!')
+    } else if (currentUserHealth <= 0) {
+        alert('You Lost!')
+    }
+}
+
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
