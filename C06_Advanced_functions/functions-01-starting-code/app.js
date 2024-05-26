24677294065b711dffd0fc2e4627326f5de3f598
vladcuzub <vladalex1989@gmail.com>
2024-05-26 23:56:42 +0200

    add logic functions

--- C06_Advanced_functions/functions-01-starting-code/app.js
+++ C06_Advanced_functions/functions-01-starting-code/app.js
@@ -1,19 +1,59 @@
+// ==================================================================================================
+// const person = {
+//     name: 'Max',               //  <======   this is a propety from a object
+//     greet: function greet(){      //  <======   a function stored in a object is called METHOD
+//         console.log('Hello Max!')
+//     }
+// }
+// person.greet();
+// ==================================================================================================
+
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame(){
-console.log('Game is starting...!')
-}
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+const DEFAULT_CHOICE = ROCK;
+
+let gameIsRunning = false;
 
 
-
-const person = {
-    name: 'Max',               //  <======   this is a propety from a object
-    greet: function greet(){      //  <======   a function stored in a object is called METHOD
-        console.log('Hello Max!')
+// ===================================  PLAYER CHOICE =================================== 
+const getPlayerChoice = function () {
+    const selection = prompt(`${ROCK}, ${PAPER} and ${SCISSORS} ? `, '').toLocaleLowerCase();
+    if (selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ) {
+        alert(`Invalid choice! by default you chose is ${DEFAULT_CHOICE}!`);
+        return DEFAULT_CHOICE;
     }
+    return selection;
+};
+// ===================================  PLAYER CHOICE =================================== 
+
+
+// ===================================  COMPUTER CHOICE =================================== 
+const getComputerChoice = function () {
+    const randomValue = Math.floor(Math.random()) * 3;
+    if (randomValue == 1) {
+        return ROCK;
+    } else if (randomValue == 2) {
+        return PAPER;
+    } else {
+        return SCISSORS
+    }
+    console.log(randomValue)
 }
+// ===================================  COMPUTER CHOICE =================================== 
 
-person.greet();
-
-
-startGameBtn.addEventListener('click',startGame)
+startGameBtn.addEventListener('click', function () {  //  <======  function anonymous
+    if (gameIsRunning) {
+        return;
+    }
+    gameIsRunning = true;
+    console.log('The Game is starting.....!')
+    const playerSelection = getPlayerChoice();
+    const computerChoice = getComputerChoice();
+    alert(`You chose ${playerSelection} !`)
+})
