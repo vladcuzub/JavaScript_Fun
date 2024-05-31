af80ac5e5c32d3583477645eaf45d31dc42ff662
vladcuzub <vladalex1989@gmail.com>
2024-05-31 23:34:31 +0200

    add exercise add_elements

--- C07_DOM/dom-assignment-start/assignment.js
+++ C07_DOM/dom-assignment-start/assignment.js
@@ -1,17 +1,13 @@
 const task = document.querySelector('#task-1');
 const task1 = document.getElementById('task-1');
-
 task.style.backgroundColor = 'black';
 task.style.color = 'white';
 
 const head = document.querySelector('title');
 const head1 = document.querySelector('head title');
-
 head1.textContent = 'Assignment - Solved!';
 
 const assigment = document.querySelector('h1');
-
 assigment.style.color = 'red';
-
 assigment.textContent = 'Assignment - Solved!';
 
